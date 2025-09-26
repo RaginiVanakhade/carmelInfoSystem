3749598e81407fa0da7baa53d302e80cdd2cbdcd
RaginiVanakhade <raginivankhade1203@gmail.com>
2025-09-26 22:45:16 +0530

    create route for login signup

--- cis/src/auth/Login.jsx
+++ cis/src/auth/Login.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+// import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { FaUserCircle } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 
 const Login = () => {
+  const navigate = useNavigate()
   return (
-    <div>
-      Login
+    <div className="body flex justify-center items-center min-h-screen bg-gradient-to-r from-gray-200 to-blue-200"> 
+     <div className="container relative w-[850px] h-[550px] bg-white rounded-[30px] shadow-[0 0 30px rgba(0,0,0,.2)">  
+      <div className='form-box'>
+        <h1>Login</h1>
+        <div className='input-box'>
+          <input type="text" placeholder="Username" required />
+          <div><FaUserCircle /></div>
+        </div>
+        <div className='input-box'>
+          <input type="password" placeholder="Password" required />
+          <div><FaLock /></div>
+        </div>
+        <div>
+          <a href="#">Forgot Password ?</a>
+        </div>
+        <button type='submit' className='login-btn'>Login</button>
+        <p>or login with social platforms</p>
+        <div>
+          <a href=""><FaGoogle /></a>
+          <a href=""><FaFacebook /></a>
+          <a href=""><FaGithub /></a>
+          <a href=""><FaLinkedin /></a>
+        </div>
+      </div>
+      <button onClick={() => navigate('/signup')}>Don't have an account ? Sign Up</button>
     </div>
+    </div>
+
   )
 }
 
--- cis/src/main.jsx
+++ cis/src/main.jsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+import Login from './auth/Login.jsx'
+import SignUp from './auth/SignUp.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <SignUp />
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
+
