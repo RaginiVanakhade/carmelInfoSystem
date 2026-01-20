9fff66f4af0626bc628d5345edd069c02811bc0b
RaginiVanakhade <raginivankhade1203@gmail.com>
2026-01-20 22:23:31 +0530

    change baseurl

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'https://rentitsupport.com/API'
+export const baseUrl = 'https://manage.rentitsupport.com/API'
 export const authkey = import.meta.env.VITE_AUTH_KEY
 
 export const AuthLogin = `${baseUrl}/Athenticate`
